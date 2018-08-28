1d7b28785ea070414fd62a87b8c95834f09a8891
jbrucker <jebrucker@gmail.com>
2018-08-28 16:56:11 +0700

    improve Javadoc, add new tests

--- src/ArrayMath.java
+++ src/ArrayMath.java
@@ -6,6 +6,15 @@
  */
 public class ArrayMath {
 
+	/**
+	 * Compute dot product of two vectors.
+	 * If the sizes differ, dot product is computed over
+	 * the minimum of the two vector lengths. That is, extra
+	 * elements from one vector are ignored.
+	 * @param x first vector to multiply
+	 * @param y second vector to multiply
+	 * @return dot-product of x and y 
+	 */
 	public static double dotProduct(double[] x, double[] y) {
 		int size = Math.min(x.length, y.length);
 		double product = 0.0;
--- test/ArrayMathTest.java
+++ test/ArrayMathTest.java
@@ -12,6 +12,35 @@ public class ArrayMathTest {
 		double result = ArrayMath.dotProduct(x, y);
 		assertEquals( x[0]*y[0], result, TOL);
 		
+		// vector of larger dimension
+		x = new double[] {1, 3, 5, 7, 9};
+		y = new double[] {-2, 0.2, 4, -1, 10.0};
+		assertEquals( 101.6, ArrayMath.dotProduct(x, y), TOL);
+		assertEquals( 101.6, ArrayMath.dotProduct(y, x), TOL);
+	}
+
+	@Test
+	public void testDotProductHugeVectors() {
+		int len = 1_000_000;
+		double[] x = new double[len];
+		double[] y = new double[len];
+		Random rand = new Random();
+		double product = 0.0;
+		for(int k=0; k<len; k++) {
+			// to avoid overflowing the product using floats for elements
+			double xk = (double) rand.nextFloat();
+			double yk = (double) rand.nextFloat();
+			x[k] = xk;
+			y[k] = yk;
+			product += xk*yk;
+		}
+		assertEquals( product, ArrayMath.dotProduct(x, y), TOL);
+		assertEquals( product, ArrayMath.dotProduct(y, x), TOL);
+	}
+
+	@Test
+	public void testDotProductLengthsNotSame() {
+		
 		x = new double[] {1, 3, 5, 7, 9};
 		y = new double[] {-2, 0.2, 4};
 		assertEquals( 18.6, ArrayMath.dotProduct(x, y), TOL);
